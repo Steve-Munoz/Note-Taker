c0d17bee2df2513621a9d7cef46d8b8d5e9d2eb9
Steve Munoz <stevomunoz@yahoo.com>
2020-07-05 20:35:34 -0500

    added routes so code handles when users visit a page, as well as API post requests

---
+++ routes/apiRoutes.js
@@ -0,0 +1,46 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var db = require("../db/db");
+var fs = require("fs");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/notes", function (req, res) {
+    res.json(db);
+  });
+
+  // API POST Requests
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/notes", function (req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    db.push(req.body);
+    res.json(true);
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function (req, res) {
+    // Empty out the arrays of data
+    tableData.length = 0;
+    waitListData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
