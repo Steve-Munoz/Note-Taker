8816540bf82bfd376a546142aeb4134e065092bb
Steve Munoz <stevomunoz@yahoo.com>
2020-07-08 22:40:07 -0500

    setup the express app to handle the data parsing

--- server.js
+++ server.js
@@ -2,13 +2,19 @@
 
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 //setting up the Express app to handle the data parsing
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+//points to our server which is a series of routes and will respond when users sist or request data from different pages
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
-  console.log("App listening on " + PORT);
+  //   console.log("App listening on " + PORT);
+  console.log("Server listening on: http://localhost:" + PORT);
 });
